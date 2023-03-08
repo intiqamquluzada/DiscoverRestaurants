2c55c50b864eed2aee188bc053604976215b8d33
intiqamquluzada <quluzadintiqam@gmail.com>
2023-03-08 23:13:54 +0400

    8mart23:13

--- booking/views.py
+++ booking/views.py
@@ -131,8 +131,7 @@ def list_view(request):
     r_type = request.GET.get("type")
 
     count = request.GET.get("count")
-    print(count)
-    print("SALAM ALEYKUMM")
+
     if rating:
         try:
             rating = int(rating)
