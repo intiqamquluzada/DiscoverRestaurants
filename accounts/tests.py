61b625168a36c6aaf9b8f8bd6abe1c593391b472
Eldar Mamedov <eldar100@Mail.ru>
2023-03-22 11:27:29 +0400

    CR tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
 # Create your tests here.
+# no tests? how do you know that your service is working well?
+# read Test Driven Development and try to use at least some of these approaches,
+# because it makes your work more reliable https://medium.com/the-andela-way/test-driven-development-with-django-ccb179171dcd
