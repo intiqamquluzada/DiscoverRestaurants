173f1e881215fd297052c9efee0ce7aa8bd4e20b
intiqamquluzada <quluzadintiqam@gmail.com>
2023-04-08 23:10:45 +0400

    nothinf

--- booking/views.py
+++ booking/views.py
@@ -168,7 +168,7 @@ def list_view(request):
     r_type = request.GET.get("type")
 
     count = request.GET.get("count")
-
+    print(count)
 
     if name:
         result = result.filter(name__icontains=name)
